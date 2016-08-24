334573cdc907bf300c871325be487c556e3f2c2d
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-08-24 12:22:07 -0400

    set up webpack, first component

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Application from './components/Application.react.js'
+// var ReactDOM = require('react-dom'); 
+
+// var Application = require('./components/Application.react');
+// var WebAPIUtils = require('./utils/WebAPIUtils');
+// WebAPIUtils.initializeStreamOfTweets();
+
+ReactDOM.render(<Application />, document.getElementById('root'));
---
+++ js/components/Application.react.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Component, PropTypes} from 'react';
+
+module.exports = class Application extends React.Component {
+  render() {
+    return <h1> Hello from React!!!</h1>
+  }
+}
